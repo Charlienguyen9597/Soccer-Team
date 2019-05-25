3d46165036807abea2cef1e68bdeb4b6529ca2b0
charlie nguyen <charlienguyen9597@gmail.com>
2019-05-25 20:48:54 +1000

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularHeroku'`, () => {
+  it(`should have as title 'charlieassignment'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angularHeroku');
+    expect(app.title).toEqual('charlieassignment');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angularHeroku!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to charlieassignment!');
   });
 });
